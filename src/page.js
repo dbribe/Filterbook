7b5edfe4d60fbf9b9db2269c04a56c8ec577e2c4
Kira <kira141996@gmail.com>
2017-04-13 01:45:09 +0300

    * Hid left and right rows in default Facebook newsfeed

--- src/page.js
+++ src/page.js
@@ -2,5 +2,5 @@
 // const mainPort = chrome.runtime.connect(extensionID, {name: "page"});
 // mainPort.onMessage.addListener((event) => {});
 
-// document.getElementById("sideNav").style.display = "none !important";
-// document.getElementById("rightCol").style.display = "none !important";
+document.getElementById("sideNav").style.display = "none";
+document.getElementById("rightCol").style.display = "none";
