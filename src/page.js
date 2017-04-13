e850d38ff3379b995315a9cb92e12179cf6329c1
Kira <kira141996@gmail.com>
2017-04-13 06:21:59 +0300

    * 1 column version, added some posts filtering

--- src/page.js
+++ src/page.js
@@ -2,8 +2,16 @@
 // const mainPort = chrome.runtime.connect(extensionID, {name: "page"});
 // mainPort.onMessage.addListener((event) => {});
 
-document.getElementById("sideNav").style.display = "none";
-document.getElementById("rightCol").style.display = "none";
+const uselessElements = ["#sideNav", "#rightCol"];
+
+setTimeout(() => {
+    for (let selector of uselessElements) {
+        const element = document.querySelector(selector);
+        element.parentNode.removeChild(element);
+    }
+});
+// document.getElementById("sideNav").style.display = "none";
+// document.getElementById("rightCol").style.display = "none";
 
 const containerStyle = {
     marginLeft: "20px",
@@ -26,38 +34,100 @@ document.body.insertBefore(leftContainer, rightContainer);
 let currentContainer = leftContainer;
 
 const mainContainer = document.getElementById("mainContainer");
-mainContainer.style.opacity = "0";
+// mainContainer.style.opacity = "0";
 
-let cnt = 0;
+// let stackElements = [];
 
-let stackElements = [];
+// setInterval(() => {
+//     const elements = mainContainer.querySelectorAll("[data-ftr]");
+//     for (let element of elements) {
+//         element.removeAttribute("data-ftr");
+//         stackElements.push(element);
+//         // if (stackElements.length > 15) {
+//         //     break;
+//         // }
+//     }
+
+//     appendElements();
+// }, 1000);
+
+// let leftHeight = 0;
+// let rightHeight = 0;
+
+// const appendElements = () => {
+//     for (let element of stackElements) {
+
+//         // element.style.display = "none";
+//         // const myElement = document.createElement("div");
+//         // myElement.innerHTML = element.innerHTML;
+//         if (valid(element)) {
+//             if (leftContainer.offsetHeight <= rightContainer.offsetHeight) {
+//                 leftContainer.appendChild(element);
+//             } else {
+//                 rightContainer.appendChild(element);
+//             }
+
+//         } else {
+//             element.parentNode.removeChild(element);
+//             console.warn("kill");
+//         }
+//     }
+
+//     stackElements = [];
+// };
+
+const valid = (element) => {
+    // if (element.querySelector("h5")) {
+    //     element.parentNode.removeChild(element);
+    //     return false;
+    // }
+    // if (element.querySelector(".fwn .fcg")) {
+    //     return false;
+    // }
+    const spanBans = [
+        " is now friends with ",
+        "SuggestedPost",
+        " liked ",
+        " likes ",
+        " commented ",
+        " are now friends",
+    ]
+    const spans = element.getElementsByTagName("span");
+    for (let span of spans) {
+        for (let spanBan of spanBans) {
+            if(span.innerText.contains(spanBan)) {
+                console.warn(spanBan);
+                return false;
+            }
+        }
+    }
+    const as = element.getElementsByTagName("a");
+    for (let a of as) {
+        if (a.innerHTML == "Sponsored") {
+            console.warn("Sponsored");
+            return false;
+        }
+    }
+    return true;
+}
 
 setInterval(() => {
+    // for (let element of leftContainer.children) {
+    //     if (!valid(element)) {
+    //         leftContainer.removeChild(element);
+    //         console.warn("kill-after");
+    //     }
+    // }
+    // for (let element of rightContainer.children) {
+    //     if (!valid(element)) {
+    //         rightContainer.removeChild(element);
+    //         console.warn("kill-after");
+    //     }
+    // }
     const elements = mainContainer.querySelectorAll("[data-ftr]");
     for (let element of elements) {
-        element.removeAttribute("data-ftr");
-        stackElements.push(element);
-        if (stackElements.length > 15) {
-            break;
+        if (!valid(element)) {
+            element.parentNode.removeChild(element);
         }
     }
-
-    appendElements();
 }, 1000);
-
-const appendElements = () => {
-    for (let element of stackElements) {
-        element.style.display = "none";
-        const myElement = document.createElement("div");
-        myElement.innerHTML = element.innerHTML;
-        if (cnt % 2 == 0) {
-            leftContainer.appendChild(myElement);
-        } else {
-            rightContainer.appendChild(myElement);
-        }
-        cnt += 1;
-        element.parentNode.removeChild(element);
-    }
-
-    stackElements = [];
-}
