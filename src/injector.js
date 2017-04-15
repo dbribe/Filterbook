ba9100b1c3d1cb8d95881bbe71d024a490817e68
Kira <kira141996@gmail.com>
2017-04-16 00:00:12 +0300

    * UI is here

--- src/injector.js
+++ src/injector.js
@@ -1,11 +1,23 @@
-const pageScript = document.createElement('script');
-pageScript.src = chrome.extension.getURL('page.js');
+const pageScript = document.createElement("script");
+pageScript.src = chrome.extension.getURL("page.js");
 pageScript.type = "text/javascript";
 
 const pageStyle = document.createElement("link");
 pageStyle.rel = "stylesheet";
-pageStyle.href = chrome.extension.getURL('page.css');
+pageStyle.href = chrome.extension.getURL("page.css");
 
-const docHead = document.head;
-docHead.appendChild(pageScript);
-docHead.appendChild(pageStyle);
+const client = new XMLHttpRequest();
+client.open('GET', chrome.extension.getURL("page.html"));
+client.onreadystatechange = () => {
+    if (client.responseText !== "") {
+        const pageHTML = document.createElement("div");
+        pageHTML.id = "filterbook";
+        pageHTML.innerHTML = client.responseText;
+        if (!document.getElementById("filterbook")) {
+            document.body.insertBefore(pageHTML, document.body.firstChild);
+            pageHTML.appendChild(pageScript);
+            pageHTML.appendChild(pageStyle);
+        }
+    }
+};
+client.send();
--- src/page.js
+++ src/page.js
@@ -1,9 +1,10 @@
 const DEBUG = false;
 
 const mainContainer = document.getElementById("mainContainer");
+let filterStarted = false;
 
-let cnt = 0;
-let totalTime = 0;
+let bannedTemplates = {};
+let bannedPremadeGroups = [];
 
 const getPromise = () => {
     return new Promise((resolve, reject) => {
@@ -28,11 +29,11 @@ const getPromise = () => {
     });
 };
 
-const find = () => {
+const filter = () => {
     getPromise().then(() => {
-        find();
+        filter();
     }, () => {
-        setTimeout(find, 4000);
+        setTimeout(filter, 4000);
     });
 };
 
@@ -56,51 +57,23 @@ const valid = (element) => {
         };
 
         const check = () => {
-            if (nameElement && nameElement.getAttribute("data-hovercard").contains("user")) {
-                solve("F");
+            for (let group of bannedPremadeGroups) {
+                if (nameElement && nameElement.getAttribute("data-hovercard").contains(group.parse)) {
+                    solve("F");
+                    return;
+                }
             }
-
-            const spanBans = [
-                " is now friends with ",
-                "Suggested Post",
-                " liked ",
-                " likes ",
-                " like ",
-                " reacted to ",
-                " commented ",
-                " replied to ",
-                " are now friends",
-                " was tagged in",
-                "People You May Know",
-                "Popular Live video",
-                " You May Like",
-                "Tell Us What You Think",
-                "'s Birthday"
-            ];
-
-            const spans = element.getElementsByTagName("span");
-            for (let span of spans) {
-                for (let spanBan of spanBans) {
-                    if(span.innerText.contains(spanBan)) {
-                        solve("F");
-                        return;
+            for (let parser in bannedTemplates) {
+                if (bannedTemplates.hasOwnProperty(parser)) {
+                    const targets = element.querySelectorAll(parser);
+                    for (let template of bannedTemplates[parser]) {
+                        for (let target of targets) {
+                            if (target.innerText.contains(template.parse)) {
+                                solve("F");
+                                return;
+                            }
+                        }
                     }
-                }
-            }
-
-            const as = element.getElementsByTagName("a");
-            for (let a of as) {
-                if (a.innerHTML == "Sponsored") {
-                    solve("F");
-                    return;
-                }
-            }
-
-            const divs = element.querySelectorAll("div._5g-l");
-            for (let div of divs) {
-                if (div.innerHTML.contains("You May Like")) {
-                    solve("F");
-                    return;
                 }
             }
         };
@@ -119,8 +92,6 @@ const valid = (element) => {
     });
 };
 
-find();
-
 const getMoreButton = () => {
     let buttons = document.querySelectorAll("span.fsxl.fcg");
     for (let button of buttons) {
@@ -136,3 +107,162 @@ const moreButton = getMoreButton();
 // setInterval(() => {
 //     moreButton.click();
 // }, 4000);
+
+let config;
+
+const changeState = (label, element) => {
+    const values = ["off", "on", "off"];
+    label.classList.remove(element.value);
+    element.value = values[values.indexOf(element.value) + 1];
+    label.classList.add(element.value);
+    config = config;
+    saveConfig();
+};
+
+const getConfig = () => {
+    return JSON.parse(localStorage.getItem("Filterbook")) || getDefaultTemplate();
+};
+
+const saveConfig = () => {
+    localStorage.setItem("Filterbook", JSON.stringify(config));
+    bannedTemplates = {};
+    for (let template of config.templates) {
+        if (template.value === "off") {
+            if (!bannedTemplates[template.selector]) {
+                bannedTemplates[template.selector] = [];
+            }
+            bannedTemplates[template.selector].push(template);
+        }
+    }
+    bannedPremadeGroups = [];
+    for (let group of config.premadeGroups) {
+        if (group.value === "off") {
+            bannedPremadeGroups.push(group);
+        }
+    }
+    if (!filterStarted) {
+        filterStarted = true;
+        filter();
+    }
+};
+
+const initMenu = () => {
+    config = getConfig();
+    saveConfig();
+
+    for (let prop in config) {
+        if (config.hasOwnProperty(prop)) {
+            for (let element of config[prop]) {
+                const label = document.createElement("label");
+                label.className = "label " + element.value;
+                label.innerHTML = element.label;
+                document.getElementById(prop).appendChild(label);
+                label.addEventListener("click", (event) => {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    changeState(label, element);
+                });
+            }
+        }
+    }
+};
+
+const getDefaultTemplate = () => {
+    return {
+        templates: [
+            {
+                label: "X is now friends with Y",
+                parse: " is now friends with ",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X and Y are now friends",
+                parse: " are now friends",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "Suggested Post",
+                parse: "Suggested Post",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X liked Y",
+                parse: " liked ",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X likes Y",
+                parse: " likes ",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X and Y like Z",
+                parse: " like ",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X reacted to Y",
+                parse: " reacted to ",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X commented on Y",
+                parse: " commented on ",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X replied to Y",
+                parse: " replied to ",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X was tagged in Y",
+                parse: "was tagged in",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "People You May Know",
+                parse: "People You May Know",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "Popular Live Video",
+                parse: "Popular Live Video",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "You May Like",
+                parse: "You May Like",
+                selector: "span, div._5g-l",
+                value: "on",
+            }, {
+                label: "Tell Us What You Think",
+                parse: "Tell Us What You Think",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "X's Birthday",
+                parse: "'s Birthday",
+                selector: "span",
+                value: "on",
+            }, {
+                label: "Sponsored",
+                parse: "Sponsored",
+                selector: "a",
+                value: "on",
+            }
+        ], premadeGroups: [
+            {
+                label: "Users",
+                parse: "user",
+                value: "on",
+            }, {
+                label: "Pages",
+                parse: "pages",
+                value: "on",
+            }
+        ], groups: []
+    };
+};
+
+initMenu();
